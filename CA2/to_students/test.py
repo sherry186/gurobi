e1f3ea74f3d1664c39cc7e94d414d500ec6f0497
sylvey <a.few.star@gmail.com>
2022-05-07 17:43:40 +0800

    stage2 also add

--- CA2/to_students/test.py
+++ CA2/to_students/test.py
@@ -1,14 +1,16 @@
 from distutils.log import error
+from huristicPaperStage1insertFixStage2 import huristicPaperStage1InsertFixStage2
 from huristicPaper import huristicPaper
 from huristic_sylvey_2_2_2 import huristic_sylvey
 from huristic1_1_2 import huristic1_1_2
 from huristicPaperStage1first import huristicPaperStage1First
 from huristicPaperStage1insert import huristicPaperStage1Insert
 from huristicPaperStage1insertFix import huristicPaperStage1InsertFix
+# from huristicPaperStage1insertFixStage2 import huri
 import csv
 
 
-root = 'C:/Users/user/gurobi/CA2/data/instance'
+root = 'C:/Users/user/gurobi/CA2/500tests/test'
 subfileName = ".csv"
 
 
@@ -98,17 +100,18 @@ subfileName = ".csv"
 #       writer.writerow([path, tardyamount, makespan])
      
     
-with open('huristicPaperStageInsertFixInstance.csv', 'w', newline='') as csvfile:
+with open('huristicPaperStageInsertFix.csv', 'w', newline='') as csvfile:
   # 建立 CSV 檔寫入器
   writer = csv.writer(csvfile)
 
   # 寫入一列資料
-  writer.writerow(['path', 'tardy', 'makespan'])
+  writer.writerow(['path', 'tardy', 'makespan', 'tardy2', 'makespan2'])
 
   tardyamount2_2_2 = 0
-  for i in range(1, 6):
+  for i in range(500):
       path = root + str(i) + subfileName
       makespan, tardyamount = 0, 0
+      makespan2, tardyamount2 = 0, 0
       print(path)
       try:
         makespan, tardyamount = huristicPaperStage1InsertFix(path)
@@ -116,8 +119,32 @@ with open('huristicPaperStageInsertFixInstance.csv', 'w', newline='') as csvfile
       except: 
         # print(error)
         continue
+
+      try:
+        makespan2, tardyamount2 = huristicPaperStage1InsertFixStage2(path)
+        print(makespan2, tardyamount2)
+      except: 
+        # print(error)
+        continue
       
-      writer.writerow([path, tardyamount, makespan])
+      writer.writerow([path, tardyamount, makespan, tardyamount2, makespan2])
+ 
+
+# with open('huristicPaperStageInsertFixStage2Instance.csv', 'w', newline='') as csvfile:
+#   # 建立 CSV 檔寫入器
+#   writer = csv.writer(csvfile)
+
+#   # 寫入一列資料
+#   writer.writerow(['path', 'tardy', 'makespan'])
+
+#   tardyamount2_2_2 = 0
+#   for i in range(1, 6):
+#       path = root + str(i) + subfileName
+#       makespan, tardyamount = 0, 0
+#       print(path)
+      
+      
+#       writer.writerow([path, tardyamount, makespan])
  
 
 
